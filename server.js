fa6e02d8a02f91a18f3136e1fbaa43c3a2745b90
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-21 13:26:01 +0100

    Add  middlewares

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000')
