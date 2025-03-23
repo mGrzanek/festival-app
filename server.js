187a836596d29f331fc41b6f2d054ffb1fbceeee
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-23 23:52:58 +0100

    Build and update server file

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const concertRouter = require('./routes/concerts.routes');
 const seatsRouter = require('./routes/seats.routes');
 const testimonialsRouter = require('./routes/testimonials.routes');
 
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use('/api', concertRouter);
 app.use('/api', seatsRouter);
 app.use('/api', testimonialsRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
-    console.log('Server is running on port: 8000')
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
 });
