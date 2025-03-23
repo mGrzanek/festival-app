f3d71857b6c552b909650effbb16e0177d37c633
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-23 21:34:01 +0100

    Update SeatChooser component and update submitForm method in OrderTicketForm component

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -11,6 +11,12 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
   
   useEffect(() => {
     dispatch(loadSeatsRequest());
+    const interval = setInterval(() => {
+      dispatch(loadSeatsRequest());
+    }, 120000);
+    return () => {
+      clearInterval(interval);
+    }
   }, [dispatch])
 
   const isTaken = (seatId) => {
